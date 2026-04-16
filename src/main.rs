1556cb40c2ba2ea6e944d16a3c5fcf3fd67af706
Feydral <lian.hable.feathertech@gmail.com>
2026-04-16 11:54:53 +0200

    feat: calculate and print fps in main loop

--- src/main.rs
+++ src/main.rs
@@ -12,50 +12,93 @@ fn main() {
 
     let font = Font::load_from_file("assets/default_font.txt", 8);
 
-    let program = r#"
-        fn main() {
-            println!("Start of program");
-    
-            let a = 10;
-            let b = 25;
-            let sum = a + b;
-    
-            println!("Calculation:");
-            println!("\ta = {}", a);
-            println!("\tb = {}", b);
-            println!("\tSum = {}", sum);
-    
-            if sum > 30 {
-                println!("Result is greater than 30");
-            } else {
-                println!("Result is less than or equal to 30");
-            }
-    
-            for i in 0..5 {
-                println!("Loop iteration {}", i);
-            }
-    
-            println!("End of program");
-        }
-    "#;
+    let program = 
+r#"
+mod canvas;
+mod input;
+mod math;
+
+use crossterm::event::KeyCode;
+
+use crate::{canvas::{Canvas, font::Font}, input::Input, math::mathi};
+
+fn main() {
+    let mut canvas = Canvas::new();
+    let mut input = Input::new();
+
+    let font = Font::load_from_file("assets/default_font.txt", 8);
+
+    let program = String::from("Hi ;)");
+
+    let mut last_frame = std::time::Instant::now();
+    let mut display_fps = 0.0; 
+    let mut fps = 0;
 
     loop {
         let _ = input.update();
-        if input.is_key_down(KeyCode::Esc) {
+        if input.is_key_pressed(KeyCode::Esc) {
             break;
         }
-    
+
+        let now = std::time::Instant::now();
+        let dt = now.duration_since(last_frame).as_secs_f32();
+        last_frame = now;
+        display_fps += dt;
+
         let new_w = Canvas::terminal_width();
         let new_h = Canvas::terminal_height();
-    
         if new_w as u32 != canvas.width() || new_h as u32 != canvas.height() {
             canvas.resize(new_w, new_h);
         }
-    
+
+        if display_fps > 1.0 {
+            display_fps = 0.0;
+            fps = (1.0 / dt) as u32;
+        }
+
         canvas.clear();
 
         canvas.draw_text(&font, 5, 5, program, mathi::rgb_to_u32(127, 127, 127));
-    
+        canvas.draw_text(&font, canvas.width() - 23, canvas.height() - 13, &fps.to_string(), mathi::rgb_to_u32(127, 127, 127));
+
+        canvas.render();
+    }
+}
+"#;
+
+    let program = program.trim_start_matches('\n');
+
+    let mut last_frame = std::time::Instant::now();
+    let mut display_fps = 0.0; 
+    let mut fps = 0;
+
+    loop {
+        let _ = input.update();
+        if input.is_key_pressed(KeyCode::Esc) {
+            break;
+        }
+
+        let now = std::time::Instant::now();
+        let dt = now.duration_since(last_frame).as_secs_f32();
+        last_frame = now;
+        display_fps += dt;
+
+        let new_w = Canvas::terminal_width();
+        let new_h = Canvas::terminal_height();
+        if new_w as u32 != canvas.width() || new_h as u32 != canvas.height() {
+            canvas.resize(new_w, new_h);
+        }
+
+        if display_fps > 1.0 {
+            display_fps = 0.0;
+            fps = (1.0 / dt) as u32;
+        }
+
+        canvas.clear();
+
+        canvas.draw_text(&font, 5, 5, program, mathi::rgb_to_u32(127, 127, 127));
+        canvas.draw_text(&font, canvas.width() - 23, canvas.height() - 13, &fps.to_string(), mathi::rgb_to_u32(127, 127, 127));
+
         canvas.render();
     }
 }
